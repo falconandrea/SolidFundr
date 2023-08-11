87c51888f54a469895a01fb129a9346f0953df2e
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-08-11 16:24:56 +0200

    feat: init project

---
+++ README.md
@@ -0,0 +1,34 @@
+<a href="https://trackgit.com">
+<img src="https://us-central1-trackgit-analytics.cloudfunctions.net/token/ping/ljpttoh8nm033ssn8qbl" alt="trackgit-views" />
+</a>
+
+# Hardhat + Next Boilerplate
+
+I created this simple Boilerplate with Hardhat + NextJS for doing my exercises and tutorials.
+
+Features:
+
+- Example Mint NFT contract + test
+- Empty frontend with default functions (wallet connect, get providers or signers, check network)
+- Hardhat and NextJS ready to code
+
+TODO:
+
+- Create simple graphic template
+
+```shell
+# Install packages
+npm install
+
+# Compile contracts
+npx hardhat compile
+
+# Launch tests
+npx hardhat test
+
+# Run script deploy
+npx hardhat run scripts/deploy.js --network goerli
+
+# Use frontend in local
+npm run dev
+```
---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox")
+require('dotenv').config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const config = {
+  solidity: "0.8.18",
+  networks: {}
+}
+
+// Check network to use
+if(process.env.GOERLI_URL_PROVIDER && process.env.GOERLI_URL_PROVIDER != '') config.networks.goerli = {
+  url: process.env.GOERLI_URL_PROVIDER,
+  accounts: [process.env.PRIVATE_KEY]
+}
+if(process.env.SEPOLIA_URL_PROVIDER && process.env.SEPOLIA_URL_PROVIDER != '') config.networks.sepolia = {
+  url: process.env.SEPOLIA_URL_PROVIDER,
+  accounts: [process.env.PRIVATE_KEY]
+}
+if(process.env.MAINNET_URL_PROVIDER && process.env.MAINNET_URL_PROVIDER != '') config.networks.mainnet = {
+  url: process.env.MAINNET_URL_PROVIDER,
+}
+
+// Etherscan apikey
+if(process.env.ETHERSCAN_API_KEY && process.env.ETHERSCAN_API_KEY) {
+  config.etherscan = {}
+  config.etherscan.apiKey = process.env.ETHERSCAN_API_KEY
+}
+
+module.exports = config;
