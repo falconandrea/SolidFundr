5a2efe454c2de5f4193e5f35717eb9112b05bb63
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-08-11 19:43:48 +0200

    feat: complete tests

--- test/SolidFundr.js
+++ test/SolidFundr.js
@@ -68,6 +68,42 @@ describe("SolidFundr", function () {
       // Check emit event
       expect(tx).to.emit(contract, "FundCreated").withArgs(0);
     });
+
+    it("Should'nt be able to create fund with amount 0", async function () {
+      const { contract, otherAccount, otherAccount2 } = await loadFixture(
+        deployFixture
+      );
+
+      // Check if tx is reverted
+      await expect(
+        contract
+          .connect(otherAccount)
+          .createFund(
+            hre.ethers.parseEther("0"),
+            otherAccount2.address,
+            "First test",
+            "First fund created"
+          )
+      ).to.be.revertedWithCustomError(contract, "AmountLessThanZero");
+    });
+
+    it("Should'nt be able to create fund with 0 address", async function () {
+      const { contract, otherAccount, otherAccount2 } = await loadFixture(
+        deployFixture
+      );
+
+      // Check if tx is reverted
+      await expect(
+        contract
+          .connect(otherAccount)
+          .createFund(
+            hre.ethers.parseEther("1"),
+            "0x0000000000000000000000000000000000000000",
+            "First test",
+            "First fund created"
+          )
+      ).to.be.revertedWithCustomError(contract, "AddressNotValid");
+    });
   });
 
   describe("Donations", function () {
@@ -118,6 +154,124 @@ describe("SolidFundr", function () {
       // Check donation is saved
       expect(donation[0]).to.be.eq(amount);
       expect(donation[1]).to.be.eq(otherAccount2.address);
+    });
+
+    it("Shoul close fund if target amount is reached", async function () {
+      const { contract, otherAccount, otherAccount2 } = await loadFixture(
+        deployFixture
+      );
+
+      const amount = hre.ethers.parseEther("1");
+
+      // Create a fund
+      const fund = await createNewFund(
+        contract,
+        otherAccount,
+        otherAccount2.address,
+        "1"
+      );
+
+      // Get balance of target user before fund is closed
+      const balanceBefore = await hre.ethers.provider.getBalance(
+        otherAccount2.address
+      );
+
+      // Do the donation
+      const tx = await contract.connect(otherAccount).donate(fund.id, {
+        value: amount,
+      });
+
+      // Check tx is not reverted
+      expect(tx).to.be.not.reverted;
+
+      // Check emit 2 events
+      expect(tx).to.emit(contract, "FundCompleted").withArgs(fund.id);
+      expect(tx)
+        .to.emit(contract, "DonationCreated")
+        .withArgs(amount, otherAccount2.address);
+
+      // Get fund
+      const funds = await contract.getFunds();
+      const updatedFund = funds[fund.id];
+
+      // Check fund is closed
+      expect(updatedFund.completed).to.be.true;
+
+      // Check target user have received the amount of the fund
+      const balanceAfter = await hre.ethers.provider.getBalance(
+        otherAccount2.address
+      );
+      expect(Number(balanceAfter)).to.be.eq(Number(balanceBefore + amount));
+    });
+
+    it("Shouldn't be able to donate to closed fund", async function () {
+      const { contract, otherAccount, otherAccount2 } = await loadFixture(
+        deployFixture
+      );
+
+      const amount = hre.ethers.parseEther("2");
+
+      // Create a fund
+      const fund = await createNewFund(
+        contract,
+        otherAccount,
+        otherAccount2.address,
+        "1"
+      );
+
+      // Do the donation to close the fund
+      const tx = await contract.connect(otherAccount).donate(fund.id, {
+        value: amount,
+      });
+
+      // Retry to do another donation
+      await expect(
+        contract.connect(otherAccount).donate(fund.id, {
+          value: amount,
+        })
+      ).to.be.revertedWithCustomError(contract, "FundAlreadyCompleted");
+    });
+
+    it("Shouldn't be able to donate amount 0", async function () {
+      const { contract, otherAccount, otherAccount2 } = await loadFixture(
+        deployFixture
+      );
+
+      // Create a fund
+      const fund = await createNewFund(
+        contract,
+        otherAccount,
+        otherAccount2.address,
+        "1"
+      );
+
+      // Check if tx is reverted
+      await expect(
+        contract.connect(otherAccount).donate(fund.id, {
+          value: hre.ethers.parseEther("0"),
+        })
+      ).to.be.revertedWithCustomError(contract, "AmountLessThanZero");
+    });
+
+    it("Shouldn't be able to donate to not existing fund", async function () {
+      const { contract, otherAccount, otherAccount2 } = await loadFixture(
+        deployFixture
+      );
+
+      // Create a fund
+      const fund = await createNewFund(
+        contract,
+        otherAccount,
+        otherAccount2.address,
+        "1"
+      );
+
+      // Check if tx is reverted
+      await expect(
+        contract.connect(otherAccount).donate(1, {
+          value: hre.ethers.parseEther("1"),
+        })
+      ).to.be.revertedWithCustomError(contract, "FundDoesNotExist");
     });
   });
 });
